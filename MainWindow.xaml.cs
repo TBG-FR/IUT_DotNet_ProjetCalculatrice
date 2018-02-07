a71dc45828c4b39cf8ef7ad08d4b049b02dc40cc
Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
2018-02-07 15:30:22 +0100

    First commit - Project & MainWindow
    - Visual Studio Project
    - MainWindow, with first elements

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetCalculatrice
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        MSScriptControl.ScriptControl sc = new MSScriptControl.ScriptControl();
+        sc.Language = "VBScript";
+        string expression = "(1 + 2) * 7";
+        object result = sc.Eval(expression);
+        MessageBox.Show(result.ToString());        
+        */
+
+    }
+}
