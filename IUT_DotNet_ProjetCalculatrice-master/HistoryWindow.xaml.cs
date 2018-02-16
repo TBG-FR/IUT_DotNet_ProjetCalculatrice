b589e5983ea829a8c1ce7b203c273f61a0f1ad38
rattlesneak <36234638+rattlesneak@users.noreply.github.com>
2018-02-16 13:37:17 +0100

    Add files via upload
    ma branche

---
+++ IUT_DotNet_ProjetCalculatrice-master/HistoryWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ProjetCalculatrice.Model;
+
+namespace ProjetCalculatrice
+{
+    /// <summary>
+    /// Logique d'interaction pour HistoryWindow.xaml
+    /// </summary>
+    public partial class HistoryWindow : Window
+    {
+        public HistoryWindow(Calculatrice calculatrice)
+        {
+            this.DataContext = calculatrice;
+            InitializeComponent();
+        }
+    }
+}
---
+++ IUT_DotNet_ProjetCalculatrice-master/Model/Calculatrice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetCalculatrice.Model
+{
+    public class Calculatrice : BaseNotifyPropertyChanged
+    {
+
+        /* ===== ===== ===== Model.Calculatrice - Attributes & Properties ===== ===== ===== */
+
+        public Calcul CurrentCalcul
+        {
+            get { return (Calcul) GetProperty(); }
+            set { SetProperty(value); }
+        }
+        public ObservableCollection<Calcul> Calculs
+        {
+            get { return (ObservableCollection<Calcul>)GetProperty(); }
+            set { SetProperty(value); }
+        }
+
+        /* ===== ===== ===== Model.Calculatrice - Constructor ===== ===== ===== */
+
+        public Calculatrice()
+        {
+
+            Calculs = new ObservableCollection<Calcul>();
+            CurrentCalcul = new Calcul();
+
+            System.Console.Write("[+] Calculatrice - Empty");
+
+        }
+
+        /* ===== ===== ===== Model.Calculatrice - Methods ===== ===== ===== */
+
+        public void Calculate(string input)
+        {
+
+            // Ajouter le Calcul précédent à l'historique, si il existe et si il a été calculé avec succès
+            if (this.CurrentCalcul != null && this.CurrentCalcul.Done == true) { Calculs.Add(this.CurrentCalcul); }
+
+            // Créer un nouveau Calcul avec l'input, puis effectuer le calcul
+            this.CurrentCalcul = new Calcul(input);
+            this.CurrentCalcul.Calculate();
+
+            // TODO - LA SUITE
+
+        }
+
+    }
+}
